eab7fb7bff9ea1528a95f5b82cca9d2f829ece5c
Go Suzuki <puyogo.suzuki@gmail.com>
2026-06-17 15:01:23 +0900

    [IMPROVE] Refactoring projgen-tool.

--- cargo-mtukai-projgen/src/main.rs
+++ cargo-mtukai-projgen/src/main.rs
@@ -1,13 +1,10 @@
 use anyhow::{Context, Result};
 use cargo_metadata::MetadataCommand;
-use std::fs::{remove_dir_all, create_dir_all};
 use std::fs;
 use std::path::{Path, PathBuf};
-use walkdir::WalkDir;
 
 mod cargo_toml;
-
-const GEN_DIR : &str = "generated";
+mod project_clone;
 
 fn get_manifest_path() -> Option<String> {
     let mut args = std::env::args().skip_while(|val| !val.starts_with("--manifest-path"));
@@ -37,6 +34,8 @@ fn get_manifest_path() -> Option<String> {
 //     println!("The offload directory is located at: {}", offload_dir.display());
 // }
 
+const GEN_DIR : &str = "generated";
+
 fn main() -> Result<()> {
     let manifest_path = get_manifest_path();
 
@@ -65,108 +64,19 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn clone_project(src: &Path, dst: &Path,  is_ignored : fn(&Path, &Path, &Path, &[PathBuf],) -> bool) -> Result<()> {
-    // if dst.exists() {
-    //     fs::remove_dir_all(dst)?;
-    // }
-
-    let blacklist = [src.join(GEN_DIR), src.join("Cargo.toml"), src.join("Cargo.lock"), src.join("target")];
-
-    if dst.exists() {
-        for entry in WalkDir::new(dst).contents_first(true) {
-            let entry = entry?;
-            let path = entry.path();
-
-            if path == dst || is_ignored(path, src, dst, &blacklist) {
-                continue;
-            }
-
-            let relative = path.strip_prefix(dst)?;
-            let source_path = src.join(relative);
-
-            if source_path.exists() && !is_ignored(&source_path, src, dst, &blacklist) {
-                continue;
-            }
-
-            if entry.file_type().is_dir() {
-                fs::remove_dir_all(path)?;
-            } else {
-                let mut perms = fs::metadata(path)?.permissions();
-                perms.set_readonly(false);
-                fs::set_permissions(path, perms)?;
-                fs::remove_file(path)?;
-            }
-        }
-    }
-
-    for entry in WalkDir::new(src)
-        .into_iter()
-        .filter_entry(|e| !(is_ignored(e.path(), src, dst, &blacklist) || e.path() == dst))
-    {
-        let entry = entry?;
-        let path = entry.path();
-
-        let relative = path.strip_prefix(src)?;
-        let target_path = dst.join(relative);
-
-        if entry.file_type().is_dir() {
-            fs::create_dir_all(&target_path)?;
-        } else {
-            if let Some(parent) = target_path.parent() {
-                fs::create_dir_all(parent)?;
-            }
-
-            // Compare modification times - only copy if source is newer than target
-            let should_copy = if target_path.exists() {
-                let target_metadata = fs::metadata(&target_path)?;
-                let source_metadata = fs::metadata(path)?;
-                
-                let target_modified = target_metadata.modified()?;
-                let source_modified = source_metadata.modified()?;
-                
-                let res = source_modified > target_modified;
-                if res {
-                    let mut perm = target_metadata.permissions();
-                    perm.set_readonly(false);
-                    fs::set_permissions(&target_path, perm)?;
-                }
-                res
-            } else {
-                true
-            };
-
-            if should_copy {
-                fs::copy(path, &target_path)
-                    .with_context(|| format!("Failed to copy {:?}", path))?;
-
-                let mut perms = fs::metadata(&target_path)?.permissions();
-                perms.set_readonly(true);
-                fs::set_permissions(&target_path, perms)?;
-            }
-        }
-    }
-
-    Ok(())
-}
-
-fn is_ignored_main(path: &Path, src: &Path, dst: &Path, blacklist: &[PathBuf]) -> bool {
+fn is_ignored_main(path: &Path, src: &Path, dst: &Path) -> bool {
     let name = path.file_name().and_then(|s| s.to_str()).unwrap_or("");
 
     if path.starts_with(src.join(".cargo")) {
         return false;
     }
-    if name == "target"
-        || name.starts_with(".")
-        || blacklist.iter().any(|p| path.starts_with(p)) {
-        return true;
-    }
-    false
+    name.starts_with(".")
 }
 
-fn gen_main_project(source: &Path, destination: &Path, cargo_toml: &cargo_toml::CargoToml) -> Result<()> {
-    let destination = &destination.join("main");
+fn gen_main_project(source: &Path, destination_origin: &Path, cargo_toml: &cargo_toml::CargoToml) -> Result<()> {
+    let destination = &destination_origin.join("main");
     // Clone the project
-    clone_project(&source, &destination, is_ignored_main)?;
+    project_clone::clone_project(&source, &destination_origin, &destination, is_ignored_main)?;
 
     // Generate Cargo.toml
     let main_cargo_toml = cargo_toml.generate_main_file()?.to_string();
@@ -174,22 +84,16 @@ fn gen_main_project(source: &Path, destination: &Path, cargo_toml: &cargo_toml::
     Ok(())
 }
 
-fn is_ignored_lp(path: &Path, src: &Path, dst: &Path, blacklist: &[PathBuf]) -> bool {
+fn is_ignored_lp(path: &Path, src: &Path, _dst: &Path) -> bool {
     let name = path.file_name().and_then(|s| s.to_str()).unwrap_or("");
 
-    if name == "target"
-        || name.starts_with(".")
-        || blacklist.iter().any(|p| path.starts_with(p))
-        || path.starts_with(src.join("build.rs")) {
-        return true;
-    }
-    false
+    name.starts_with(".") || path.starts_with(src.join("build.rs"))
 }
 
-fn gen_lp_project(source: &Path, destination: &Path, cargo_toml: &cargo_toml::CargoToml) -> Result<()> {
-    let destination = &destination.join("lp");
+fn gen_lp_project(source: &Path, destination_origin: &Path, cargo_toml: &cargo_toml::CargoToml) -> Result<()> {
+    let destination = &destination_origin.join("lp");
     // Clone the project
-    clone_project(&source, &destination, is_ignored_lp)?;
+    project_clone::clone_project(&source, &destination_origin, &destination, is_ignored_lp)?;
 
     // Generate Cargo.toml
     let lp_cargo_toml = cargo_toml.generate_lp_file()?.to_string();
---
+++ cargo-mtukai-projgen/src/project_clone.rs
@@ -0,0 +1,87 @@
+use anyhow::{Context, Result};
+use std::path::{Path, PathBuf};
+use std::fs;
+use walkdir::WalkDir;
+
+
+pub fn clone_project(src: &Path, dst_origin: &Path, dst: &Path,  is_ignored : fn(&Path, &Path, &Path) -> bool) -> Result<()> {
+    // if dst.exists() {
+    //     fs::remove_dir_all(dst)?;
+    // }
+    let blacklist = [src.join("Cargo.toml"), src.join("Cargo.lock"), src.join("target")];
+
+    let file_filter = |e: &walkdir::DirEntry| {
+        let path = e.path();
+        !blacklist.iter().any(|p| path.starts_with(p)) // The file is not in the blacklist.
+        && !is_ignored(path, src, dst) // The file is not ignored.
+        && path != dst // Unexpected state. leave it.
+    };
+    if dst.exists() {
+        for entry in
+            WalkDir::new(dst).contents_first(true)
+            .into_iter()
+            .filter_entry(file_filter){
+            let entry = entry?;
+            let path = entry.path();
+
+            if src.join(path.strip_prefix(dst)?).exists() { // The source files exists, do not delete it.
+                continue; // Ignored file.
+            }
+
+            if entry.file_type().is_dir() {
+                fs::remove_dir_all(path)?;
+            } else {
+                let mut perms = fs::metadata(path)?.permissions();
+                perms.set_readonly(false);
+                fs::set_permissions(path, perms)?;
+                fs::remove_file(path)?;
+            }
+        }
+    }
+
+    for entry in WalkDir::new(src)
+        .into_iter()
+        .filter_entry(|e| file_filter(e) && !e.path().starts_with(dst_origin)) {
+        let entry = entry?;
+        let path = entry.path();
+        let relative = path.strip_prefix(src)?;
+        let target_path = dst.join(relative);
+
+        if entry.file_type().is_dir() {
+            fs::create_dir_all(&target_path)?;
+        } else {
+            if let Some(parent) = target_path.parent() {
+                fs::create_dir_all(parent)?;
+            }
+
+            // Compare modification times - only copy if source is newer than target
+            let should_copy = if target_path.exists() {
+                let target_metadata = fs::metadata(&target_path)?;
+                let source_metadata = fs::metadata(path)?;
+                
+                let target_modified = target_metadata.modified()?;
+                let source_modified = source_metadata.modified()?;
+                
+                let res = source_modified > target_modified;
+                if res {
+                    let mut perm = target_metadata.permissions();
+                    perm.set_readonly(false);
+                    fs::set_permissions(&target_path, perm)?;
+                }
+                res
+            } else {
+                true
+            };
+
+            if should_copy {
+                fs::copy(path, &target_path)
+                    .with_context(|| format!("Failed to copy {:?}", path))?;
+
+                let mut perms = fs::metadata(&target_path)?.permissions();
+                perms.set_readonly(true);
+                fs::set_permissions(&target_path, perms)?;
+            }
+        }
+    }
+    Ok(())
+}
